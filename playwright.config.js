55ee1047fc36168b172de66fe6cdc4ee19672147
ravigitgit <ravitiwari31415@gmail.com>
2025-07-16 19:16:20 +0530

    changes in config & actions

--- playwright.config.js
+++ playwright.config.js
@@ -30,9 +30,9 @@ export default defineConfig({
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  timeout: 60 * 1000,
+  timeout: 100 * 1000,
   expect: {
-    timeout: 60 * 1000
+    timeout: 100 * 1000
   },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
